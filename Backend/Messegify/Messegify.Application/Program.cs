7ea76ff59e002658e282cb18b7d86bbb431fc0d3
Bartek Banaś <bartek.banas675@gmail.com>
2022-12-29 14:11:40 +0100

    Added database connection

--- Backend/Messegify/Messegify.Application/Program.cs
+++ Backend/Messegify/Messegify.Application/Program.cs
@@ -1,11 +1,22 @@
+using Messegify.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
+var configuration = builder.Configuration;
+
+// Add services to the container.
+var services = builder.Services;
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddDbContext<MessegifyDbContext>(contextOptionsBuilder =>
+    contextOptionsBuilder.UseSqlServer(configuration.GetConnectionString("MessegifyDatabaseConnectionString")));
+
+
 
 var app = builder.Build();
 
---
+++ Backend/Messegify/Messegify.Infrastructure/MessegifyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Messegify.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Messegify.Infrastructure;
+
+public class MessegifyDbContext : DbContext
+{
+    public MessegifyDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<AccountRoom> AccountRooms { get; set; }
+    public DbSet<Friendship> Friendships { get; set; }
+}
