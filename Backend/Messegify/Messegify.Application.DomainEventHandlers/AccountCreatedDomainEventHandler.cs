c6ae60f46116aa98291b913bd63972cb0bc8aa04
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-04 18:20:34 +0100

    missing from last commit

---
+++ Backend/Messegify/Messegify.Application.DomainEventHandlers/AccountCreatedDomainEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Messegify.Domain.Abstractions;
+using Messegify.Domain.Entities;
+using Messegify.Domain.Events;
+
+namespace Messegify.Application.DomainEventHandlers;
+
+public class AccountCreatedDomainEventHandler : INotificationHandler<AccountCreatedDomainEvent>
+{
+    private readonly IRepository<User> _userRepository;
+
+    public AccountCreatedDomainEventHandler(IRepository<User> userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task Handle(AccountCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var newUser = new User()
+        {
+            AccountId = notification.Entity.Id,
+            Username = notification.Entity.Name,
+        };
+
+        await _userRepository.CreateAsync(newUser);
+    }
+}
