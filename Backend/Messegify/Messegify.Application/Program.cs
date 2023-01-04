9390b9593ff7356abd4f45e0cab7e092068ee905
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-04 16:59:07 +0100

    added authentication

--- Backend/Messegify/Messegify.Application/Program.cs
+++ Backend/Messegify/Messegify.Application/Program.cs
@@ -1,20 +1,23 @@
+using System.Text;
 using FluentValidation;
-using FluentValidation.AspNetCore;
 using Messegify.Application.Middleware;
 using Messegify.Application.Services;
 using Messegify.Application.Services.Configuration;
 using Messegify.Domain.Abstractions;
 using Messegify.Domain.Entities;
-using Messegify.Domain.Validators;
 using Messegify.Infrastructure;
 using Messegify.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 var configuration = builder.Configuration;
+
+// Settings
+var jwtConfig = configuration.GetRequiredSection("JwtConfiguration").Get<JwtConfiguration>();
 
 // Add services to the container.
 var services = builder.Services;
@@ -25,6 +28,26 @@ services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = jwtConfig.Issuer,
+        ValidAudience = jwtConfig.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(jwtConfig.SecretKey)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true // TODO: maybe will create problems later
+    };
+});
 
 services.AddDbContext<MessegifyDbContext>(contextOptionsBuilder =>
     contextOptionsBuilder.UseMySQL(configuration.GetConnectionString("MessegifyDatabaseConnectionString")));
@@ -51,6 +74,8 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
