b7574d4114ba2055009643d378fcfd34e23af2c9
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-17 17:38:38 +0100

    More nullifers

--- Backend/Messegify/Messegify.Application.Configuration/JwtConfiguration.cs
+++ Backend/Messegify/Messegify.Application.Configuration/JwtConfiguration.cs
@@ -2,10 +2,10 @@
 
 public class JwtConfiguration
 {
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
-    public string PublicKey { get; set; }
-    public string PublicKeyInfo { get; set; }
-    public string SecretKey { get; set; }
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string PublicKey { get; set; } = null!;
+    public string PublicKeyInfo { get; set; } = null!;
+    public string SecretKey { get; set; } = null!;
     public int Expires { get; set; }
 }
