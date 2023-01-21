eaac1d5aef1b1c22d644c6cdd4cb9a03381a1a8b
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-22 00:04:50 +0100

    Environmental variables

--- Front-end/messegify/src/features/login/api.ts
+++ Front-end/messegify/src/features/login/api.ts
@@ -1,5 +1,7 @@
+import {API_URL} from "../../config";
+
 export const login = async (username: string, password: string) => {
-    const response = await fetch('http://localhost:5000/auth/login', {
+    const response = await fetch(`${API_URL}/auth/login`, {
         method: 'POST',
         headers: {
             ContentType: 'application/json',
