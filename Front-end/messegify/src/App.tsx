e2c6b67f8da8ccd0ea587404d35dc30426ece1fa
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-21 21:57:07 +0100

    Importing Mantine

--- Front-end/messegify/src/App.tsx
+++ Front-end/messegify/src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import {Routing} from "./pages/Routing";
+import {MantineProvider} from "@mantine/core";
+import {NotificationsProvider} from "@mantine/notifications";
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Routing/>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <NotificationsProvider>
+                    <Routing/>
+                </NotificationsProvider>
+            </MantineProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
