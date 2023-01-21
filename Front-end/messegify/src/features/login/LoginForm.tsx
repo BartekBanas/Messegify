48bbc2e0bc5337cfd9bbacac8ad9aaecdd6ef67e
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-22 00:16:03 +0100

    Not yet appropriate login

--- Front-end/messegify/src/features/login/LoginForm.tsx
+++ Front-end/messegify/src/features/login/LoginForm.tsx
@@ -2,8 +2,12 @@ import {useForm} from "@mantine/form";
 import {FC} from "react";
 import {LoginFormType} from "./login-form.types";
 import {Stack, TextInput, Button} from "@mantine/core";
+import {login} from "./api";
+import {loginErrorNotification} from "./notifications";
+import {useNavigate} from "react-router-dom";
 
 export const LoginForm: FC = () => {
+    const navigate = useNavigate();
     const form = useForm<LoginFormType>({
         initialValues: {
             email: '',
@@ -12,14 +16,19 @@ export const LoginForm: FC = () => {
     })
 
     async function handleSubmit(data: LoginFormType) {
-        
+        try {
+            await login(data.email, data.password);
+            navigate('/menu');
+        } catch (error) {
+            loginErrorNotification();
+        }
     }
 
     return (
         <form onSubmit={form.onSubmit(values => handleSubmit(values))}>
             <Stack spacing="md">
-                <TextInput required type={"email"} label="Email" {...form.getInputProps('email')}/>
-                <TextInput required type={"password"} label="Password" {...form.getInputProps('password')}/>
+                <TextInput required type="email" label="Email" {...form.getInputProps('email')}/>
+                <TextInput required type="password" label="Password" {...form.getInputProps('password')}/>
                 <Button type="submit">Login</Button>
             </Stack>
         </form>
