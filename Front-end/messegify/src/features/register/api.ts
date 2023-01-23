db1d7d864046b2e893ad7095902b45b2dd12c14c
Bartek Banaś <bartek.banas675@gmail.com>
2023-01-23 23:39:41 +0100

    Changing send request

--- Front-end/messegify/src/features/register/api.ts
+++ Front-end/messegify/src/features/register/api.ts
@@ -1,13 +1,24 @@
 import {API_URL} from "../../config";
+import {RegisterFormType} from "./register-form.type";
 
-export const register = async (username: string, password: string, email: string) => {
+export const register = async (Username: string, Password: string, Email: string) => {\
+
+    var registerAccountDto: RegisterFormType;
+    registerAccountDto = {
+        email: Email,
+        password: Password,
+        username: Username
+    }
+
     const response = await fetch(`${API_URL}/account`, {
         method: 'POST',
         headers: {
             ContentType: 'application/json',
-            Authorization: 'Basic' + window.btoa(username + ':' + password + ':' + email),
+            // Authorization: 'Basic' + window.btoa(Username + ':' + Password + ':' + Email),
+            'Access-Control-Allow-Origin': '*'
         },
-        credentials: 'include'
+        credentials: 'include',
+        body: registerAccountDto // TODO  serialzie this
     });
 
     if (response.status !== 200) throw new Error('Registration failed');
