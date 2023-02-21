15350c8874c9757f9ea4973bc2677f2f9fb467ba
Bartek Banaś <bartek.banas675@gmail.com>
2023-02-21 22:54:04 +0100

    chatroom\api.ts cleanup

--- Front-end/messegify/src/features/chatroom/api.ts
+++ Front-end/messegify/src/features/chatroom/api.ts
@@ -1,17 +1,13 @@
-import {useState} from "react";
 import Cookies from "js-cookie";
 import ky from "ky";
 import {API_URL} from "../../config";
 import {Message} from "../../types/message";
 
 export function useGetMessages() {
-    const [messages, setMessages] = useState<Message[]>([]);
-
     const currentUrl = window.location.href;
     const roomId = currentUrl.split('/').pop();
     console.log(roomId);
-
-
+    
     const token = Cookies.get('auth_token');
     const authorizedKy = ky.extend({
         headers: {
