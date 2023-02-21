ca126c953ed254b55224fbe3096d9462609dd90d
Bartek Banaś <bartek.banas675@gmail.com>
2023-02-22 00:54:56 +0100

    Trying to add a socket

--- Front-end/messegify/src/features/chatroom/ChatroomForm.tsx
+++ Front-end/messegify/src/features/chatroom/ChatroomForm.tsx
@@ -2,6 +2,7 @@ import React, {FC, useState, useEffect} from 'react';
 import {useGetMessages} from './api';
 import {Message} from '../../types/message';
 import {MantineProvider, Paper} from '@mantine/core';
+import io from 'socket.io-client';
 
 export const ChatroomForm: FC = () => {
     const [messages, setMessages] = useState<Message[]>([]);
@@ -11,10 +12,24 @@ export const ChatroomForm: FC = () => {
         async function fetchData() {
             const receivedMessages = await getMessages;
             setMessages(receivedMessages);
+
+            console.log(receivedMessages);
         }
 
         fetchData();
-    }, [getMessages]);
+    }, []);
+
+    useEffect(() => {
+        const socket = io("https://localhost:5000/api"); // adres hosta socket.io
+
+        socket.on('newMessage', (message: Message) => {
+            setMessages([...messages, message]);
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, [messages]);
 
     return (
         <MantineProvider theme={{colorScheme: 'dark'}}>
--- Front-end/messegify/src/features/chatroom/api.ts
+++ Front-end/messegify/src/features/chatroom/api.ts
@@ -2,12 +2,14 @@ import Cookies from "js-cookie";
 import ky from "ky";
 import {API_URL} from "../../config";
 import {Message} from "../../types/message";
+import {useState} from "react";
+import useWebSocket from "react-use-websocket";
 
 export function useGetMessages() {
     const currentUrl = window.location.href;
     const roomId = currentUrl.split('/').pop();
-    console.log(roomId);
-    
+    const endpoint = new URL('/board/all', API_URL).href
+
     const token = Cookies.get('auth_token');
     const authorizedKy = ky.extend({
         headers: {
@@ -16,4 +18,31 @@ export function useGetMessages() {
     });
 
     return authorizedKy.get(`${API_URL}/chatRoom/${roomId}/message/list`).json<Message[]>();
+}
+
+export function useMessageWebSocket() {
+    const currentUrl = window.location.href;
+    const roomId = currentUrl.split('/').pop();
+    const apiUri = process.env.REACT_APP_API_URL;
+    const hostname = new URL('/chatRoom/' + {roomId} + '/message', apiUri).host;
+    const endpoint = `wss://${hostname}/ws`;
+    const [socketUrl,] = useState(endpoint);
+
+    const {
+        sendMessage,
+        lastMessage,
+        readyState,
+        getWebSocket,
+        lastJsonMessage,
+        sendJsonMessage
+    } = useWebSocket(socketUrl);
+
+    return {
+        sendMessage,
+        lastMessage,
+        readyState,
+        getWebSocket,
+        lastJsonMessage,
+        sendJsonMessage
+    }
 }
