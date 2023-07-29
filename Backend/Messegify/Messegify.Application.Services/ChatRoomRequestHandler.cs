13f326e42d74504ceb185eba3ca7696b07b6929f
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-29 17:12:12 +0200

    Code cleanup

--- Backend/Messegify/Messegify.Application.Services/ChatRoomRequestHandler.cs
+++ Backend/Messegify/Messegify.Application.Services/ChatRoomRequestHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using AutoMapper;
 using MediatR;
 using Messegify.Application.Dtos;
@@ -8,14 +7,13 @@ using Messegify.Application.Services.ChatRoomRequests;
 using Messegify.Domain.Abstractions;
 using Messegify.Domain.Entities;
 using Messegify.Domain.Events;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 
 namespace Messegify.Application.Services;
 
-public interface IChatRoomRequestHandler 
-    : IRequestHandler<CreateChatRoomRequest>,
-        IRequestHandler<GetUserChatRooms, IEnumerable<ChatRoomDto>>
+public interface IChatRoomRequestHandler :
+    IRequestHandler<CreateChatRoomRequest>,
+    IRequestHandler<GetUserChatRooms, IEnumerable<ChatRoomDto>>
 {
 }
 
@@ -23,29 +21,25 @@ public class ChatRoomRequestHandler : IChatRoomRequestHandler
 {
     private readonly IRepository<ChatRoom> _chatRoomRepository;
     private readonly IRepository<Account> _accountRepository;
-    
-    private readonly IAuthorizationService _authorizationService;
 
     private readonly IHttpContextAccessor _httpContextAccessor;
 
     private readonly IMapper _mapper;
 
     public ChatRoomRequestHandler(
-        IRepository<ChatRoom> chatRoomRepository, 
-        IRepository<Account> accountRepository, 
-        IAuthorizationService authorizationService, 
-        IHttpContextAccessor httpContextAccessor, 
+        IRepository<ChatRoom> chatRoomRepository,
+        IRepository<Account> accountRepository,
+        IHttpContextAccessor httpContextAccessor,
         IMapper mapper)
     {
         _chatRoomRepository = chatRoomRepository;
         _accountRepository = accountRepository;
-        _authorizationService = authorizationService;
         _httpContextAccessor = httpContextAccessor;
         _mapper = mapper;
     }
 
     public async Task<Unit> Handle(
-        CreateChatRoomRequest request, 
+        CreateChatRoomRequest request,
         CancellationToken cancellationToken)
     {
         var userId = _httpContextAccessor.HttpContext.User.GetId();
@@ -60,17 +54,17 @@ public class ChatRoomRequestHandler : IChatRoomRequestHandler
         await _chatRoomRepository.CreateAsync(newChatRoom);
 
         newChatRoom.AddDomainEvent(new ChatRoomCreatedDomainEvent(newChatRoom, account));
-        
+
         await _chatRoomRepository.SaveChangesAsync();
-        
+
         return Unit.Value;
     }
-    
+
     public async Task<IEnumerable<ChatRoomDto>> Handle(GetUserChatRooms request, CancellationToken cancellationToken)
     {
         var userId = _httpContextAccessor.HttpContext.User.GetId();
-        
-        Expression<Func<ChatRoom, bool>> filter = chatRoom 
+
+        Expression<Func<ChatRoom, bool>> filter = chatRoom
             => chatRoom.Members.Any(membership => membership.AccountId == userId);
 
         var chatRooms = await _chatRoomRepository
