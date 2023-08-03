a98f77a83ad54a6ac203d30521618c5ea5e7e8c0
Bartek Banaś <bartek.banas675@gmail.com>
2023-08-03 23:54:14 +0200

    Apply notificationStyles

--- Frontend/messegify/src/features/login/notifications.ts
+++ Frontend/messegify/src/features/login/notifications.ts
@@ -1,25 +1,11 @@
 import {showNotification} from "@mantine/notifications";
-import {MantineProvider} from '@mantine/core';
+import notificationStyles from "../../styles/notificationStyles";
 
 export const loginErrorNotification = () => {
     showNotification({
         color: 'red',
         title: 'Error',
         message: 'Login failed',
-        styles: (theme) => ({
-            root: {
-                backgroundColor: theme.colors.dark[6],
-                borderColor: theme.colors.dark[7],
-
-                '&::before': {backgroundColor: theme.colorScheme},
-            },
-
-            title: {color: theme.colors.gray[2]},
-            description: {color: theme.colorScheme},
-            closeButton: {
-                color: theme.colorScheme,
-                '&:hover': {backgroundColor: theme.colors.blue[7]},
-            },
-        }),
+        styles: notificationStyles,
     });
 }
--- Frontend/messegify/src/features/menu/notification.ts
+++ Frontend/messegify/src/features/menu/notification.ts
@@ -1,24 +1,11 @@
 import {showNotification} from "@mantine/notifications";
+import notificationStyles from "../../styles/notificationStyles";
 
 export const MissingUserNotification = () => {
     showNotification({
         color: 'red',
         title: 'Error',
         message: 'Login failed',
-        styles: (theme) => ({
-            root: {
-                backgroundColor: theme.colors.dark[6],
-                borderColor: theme.colors.dark[7],
-
-                '&::before': {backgroundColor: theme.colorScheme},
-            },
-
-            title: {color: theme.colors.gray[2]},
-            description: {color: theme.colorScheme},
-            closeButton: {
-                color: theme.colorScheme,
-                '&:hover': {backgroundColor: theme.colors.blue[7]},
-            },
-        }),
+        styles: notificationStyles,
     })
 }
