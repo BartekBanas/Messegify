eebf3a83172b15dbd4699a736758ac0711ce684f
Bartek Banaś <bartek.banas675@gmail.com>
2023-08-03 03:17:31 +0200

    Temporarily disable SpotlightProvider from the app

--- Frontend/messegify/src/App.tsx
+++ Frontend/messegify/src/App.tsx
@@ -4,18 +4,15 @@ import {BrowserRouter} from "react-router-dom";
 import {Routing} from "./pages/Routing";
 import {MantineProvider} from "@mantine/core";
 import {NotificationsProvider} from "@mantine/notifications";
-import {SpotlightProvider} from "@mantine/spotlight";
 
 function App() {
     return (
         <MantineProvider theme={{colorScheme: 'dark'}}>
             <BrowserRouter>
                 <MantineProvider withGlobalStyles withNormalizeCSS>
-                    <SpotlightProvider actions={[]}>
-                        <NotificationsProvider>
-                            <Routing/>
-                        </NotificationsProvider>
-                    </SpotlightProvider>
+                    <NotificationsProvider>
+                        <Routing/>
+                    </NotificationsProvider>
                 </MantineProvider>
             </BrowserRouter>
         </MantineProvider>
