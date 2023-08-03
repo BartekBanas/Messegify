00cc46cd1baa2a75c2ad0cf1266f8929ba9ea02b
Bartek Banaś <bartek.banas675@gmail.com>
2023-08-04 01:02:50 +0200

    Add delete account button to drawer

--- Frontend/messegify/src/features/Messegify/components/drawer/UtilityDrawer.tsx
+++ Frontend/messegify/src/features/Messegify/components/drawer/UtilityDrawer.tsx
@@ -3,17 +3,19 @@ import {useDisclosure} from "@mantine/hooks";
 import {Drawer, Group, ActionIcon} from "@mantine/core";
 import {ContactMaker} from "../contactMaker/ContactMaker";
 import {IconAdjustments} from '@tabler/icons-react';
+import {DeleteAccountButton} from "./deleteAccountButton";
 
 interface UtilityDrawerProps {
 }
 
-export const UtilityDrawer: FC<UtilityDrawerProps> = ({}) => {
+export const UtilityDrawer: FC<UtilityDrawerProps> = () => {
     const [opened, {open, close}] = useDisclosure(false);
 
     return (
         <>
             <Drawer opened={opened} onClose={close} title="Authentication">
                 <ContactMaker/>
+                <DeleteAccountButton/>
             </Drawer>
 
             <Group position="center">
