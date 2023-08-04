0ebb79d3e3a5e7a20ec3c811664d031e36b9cfc3
Bartek Banaś <bartek.banas675@gmail.com>
2023-08-04 17:30:46 +0200

    Add validating uniqueness of account name

--- Backend/Messegify/Messegify.Domain.Validators/AccountValidator.cs
+++ Backend/Messegify/Messegify.Domain.Validators/AccountValidator.cs
@@ -1,14 +1,32 @@
 ﻿using FluentValidation;
+using Messegify.Domain.Abstractions;
 using Messegify.Domain.Entities;
+using Messegify.Infrastructure.Error;
 
 namespace Messegify.Domain.Validators;
 
 public class AccountValidator : AbstractValidator<Account>
 {
-    public AccountValidator()
+    private readonly IRepository<Account> _accountRepository;
+
+    public AccountValidator(IRepository<Account> accountRepository)
     {
+        _accountRepository = accountRepository;
+        
         RuleFor(account => account.Email).EmailAddress().NotEmpty();
         RuleFor(account => account.Name).Length(3, 32).NotEmpty();
         RuleFor(account => account.PasswordHash).NotEmpty();
+        RuleFor(account => account).CustomAsync(ValidateUniqueName);
+    }
+    
+    private async Task ValidateUniqueName(Account account, ValidationContext<Account> context, CancellationToken token)
+    {
+        var matchingAccounts = await _accountRepository
+            .GetAsync(foundAccount => foundAccount.Name == account.Name);
+
+        if (matchingAccounts.Any())
+        {
+            throw new ItemDuplicatedErrorException("Account with that username already exists");
+        }
     }
 }
