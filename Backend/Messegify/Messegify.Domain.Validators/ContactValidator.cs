16dd37c1d1fa6fcfcf4b7a23f355f64b06462608
Bartek Banaś <bartek.banas675@gmail.com>
2023-08-04 23:18:05 +0200

    Add Separate validator for contacts

---
+++ Backend/Messegify/Messegify.Domain.Validators/ContactValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Messegify.Domain.Abstractions;
+using Messegify.Domain.Entities;
+using Messegify.Infrastructure.Error;
+
+namespace Messegify.Domain.Validators;
+
+public class ContactValidator : AbstractValidator<Contact>
+{
+    private readonly IRepository<Contact> _contactRepository;
+
+    public ContactValidator(IRepository<Contact> contactRepository)
+    {
+        _contactRepository = contactRepository;
+
+        RuleFor(contact => contact).CustomAsync(ValidateUniqueContact);
+    }
+    
+    private async Task ValidateUniqueContact(Contact contact, ValidationContext<Contact> context, CancellationToken token)
+    {
+        var contacts = await _contactRepository
+            .GetAsync(foundContact => (foundContact.FirstAccountId == contact.FirstAccountId &&
+                                       foundContact.SecondAccountId == contact.SecondAccountId)
+                                 || (foundContact.FirstAccountId == contact.SecondAccountId &&
+                                     foundContact.SecondAccountId == contact.FirstAccountId));
+
+        if (contacts.Any())
+        {
+            throw new ItemDuplicatedErrorException("Contact already exists");
+        }
+    }
+}
