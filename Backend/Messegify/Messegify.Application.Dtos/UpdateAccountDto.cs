b666cd061759a46cb29b118f134680ae3212b1d3
Bartek Banaś <bartek.banas675@gmail.com>
2023-08-06 17:27:18 +0200

    Update UpdateAccountDto

--- Backend/Messegify/Messegify.Application.Dtos/UpdateAccountDto.cs
+++ Backend/Messegify/Messegify.Application.Dtos/UpdateAccountDto.cs
@@ -2,7 +2,7 @@
 
 public class UpdateAccountDto
 {
-    public string Name { get; set; } = null!;
+    public string? Name { get; set; }
     
-    public string Email { get; set; } = null!;
+    public string? Email { get; set; }
 }
