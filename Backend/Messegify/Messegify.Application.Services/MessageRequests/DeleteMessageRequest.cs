1d60f834acd71987357a93d4747c40fa992df501
Bartek Banaś <bartek.banas675@gmail.com>
2023-08-12 20:14:47 +0200

    Add DeleteMessageRequest

---
+++ Backend/Messegify/Messegify.Application.Services/MessageRequests/DeleteMessageRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Messegify.Application.Services.MessageRequests;
+
+public class DeleteMessageRequest : IRequest
+{
+    public Guid MessageId { get; set; }
+
+    public DeleteMessageRequest(Guid messageId)
+    {
+        MessageId = messageId;
+    }
+}
