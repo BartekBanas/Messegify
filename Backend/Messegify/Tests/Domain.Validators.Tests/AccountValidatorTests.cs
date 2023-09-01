398db6f75c06d74046cd50447defd6357e0eebae
Bartek Banaś <bartek.banas675@gmail.com>
2023-09-01 03:15:08 +0200

    Add tests for failed account validation

--- Backend/Messegify/Tests/Domain.Validators.Tests/AccountValidatorTests.cs
+++ Backend/Messegify/Tests/Domain.Validators.Tests/AccountValidatorTests.cs
@@ -3,6 +3,7 @@ using FluentValidation.TestHelper;
 using Messegify.Domain.Abstractions;
 using Messegify.Domain.Entities;
 using Messegify.Domain.Validators;
+using Messegify.Infrastructure.Error;
 using Moq;
 
 namespace Domain.Validators.Tests;
@@ -32,5 +33,45 @@ public class AccountValidatorTests
         result.ShouldNotHaveValidationErrorFor(acc => acc.Email);
         result.ShouldNotHaveValidationErrorFor(acc => acc.Name);
         result.ShouldNotHaveValidationErrorFor(acc => acc.PasswordHash);
+    }
+
+    [Fact]
+    public async Task Validate_DuplicateName_ThrowsItemDuplicatedErrorException()
+    {
+        // Arrange
+        var existingAccounts = new List<Account>
+        {
+            new Account { Name = "ExistingUser" }
+        };
+
+        var accountRepository = new Mock<IRepository<Account>>();
+        accountRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Account, bool>>>(), null))
+            .ReturnsAsync(existingAccounts);
+
+        var validator = new AccountValidator(accountRepository.Object);
+        var account = new Account { Name = "ExistingUser" };
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ItemDuplicatedErrorException>(async () => await validator.TestValidateAsync(account));
+    }
+
+    [Fact]
+    public async Task Validate_DuplicateEmail_ValidationErrors()
+    {
+        // Arrange
+        var existingAccounts = new List<Account>
+        {
+            new Account { Email = "existing@example.com" }
+        };
+
+        var accountRepository = new Mock<IRepository<Account>>();
+        accountRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Account, bool>>>(), null))
+            .ReturnsAsync(existingAccounts);
+
+        var validator = new AccountValidator(accountRepository.Object);
+        var account = new Account { Email = "existing@example.com" };
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ItemDuplicatedErrorException>(async () => await validator.TestValidateAsync(account));
     }
 }
