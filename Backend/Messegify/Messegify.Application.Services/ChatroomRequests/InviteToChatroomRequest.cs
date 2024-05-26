ad276ac50ce0f1836f6f52040fef42d4572bed31
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-05-26 20:41:06 +0200

    Add MediatR InviteToChatroom Request

---
+++ Backend/Messegify/Messegify.Application.Services/ChatroomRequests/InviteToChatroomRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Messegify.Application.Dtos;
+
+namespace Messegify.Application.Services.ChatroomRequests;
+
+public class InviteToChatroomRequest : IRequest
+{
+    public Guid ChatroomId { get; set; }
+    public Guid AccountId { get; set; }
+
+    public InviteToChatroomRequest(ChatroomInvite chatroomInvite)
+    {
+        ChatroomId = chatroomInvite.ChatroomId;
+        AccountId = chatroomInvite.AccountId;
+    }
+}
