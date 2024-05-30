eb93915a9ca836e1774405a19409d36d393f9b00
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-05-30 19:09:50 +0200

    Add IsOwnerOf Requirement handling for chatrooms

--- Backend/Messegify/Messegify.Application.Authorization/Handlers/ChatRoomAuthorizationHandler.cs
+++ Backend/Messegify/Messegify.Application.Authorization/Handlers/ChatRoomAuthorizationHandler.cs
@@ -5,17 +5,51 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Messegify.Application.Authorization.Handlers;
 
-public class ChatRoomAuthorizationHandler : AuthorizationHandler<IsMemberOfRequirement, Chatroom>
+public class ChatRoomAuthorizationHandler : AuthorizationHandler<IAuthorizationRequirement, Chatroom>
 {
     protected override Task HandleRequirementAsync(
-        AuthorizationHandlerContext context, 
+        AuthorizationHandlerContext context,
+        IAuthorizationRequirement requirement,
+        Chatroom resource)
+    {
+        return requirement switch
+        {
+            IsMemberOfRequirement isMemberOfRequirement => HandleIsMemberOfRequirementAsync(
+                context, isMemberOfRequirement, resource),
+
+            IsOwnerRequirement isOwnerRequirement => HandleIsOwnerOfRequirementAsync(
+                context, isOwnerRequirement, resource),
+
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    private Task HandleIsMemberOfRequirementAsync(
+        AuthorizationHandlerContext context,
         IsMemberOfRequirement requirement,
         Chatroom resource)
     {
         var user = context.User;
-        var userId = user.Claims.First(x => x.Type == ClaimTypes.PrimarySid).Value;
+        var userId = user.Claims.First(claim => claim.Type == ClaimTypes.PrimarySid).Value;
 
         if (resource.Members.Any(member => member.AccountId == new Guid(userId)))
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private Task HandleIsOwnerOfRequirementAsync(
+        AuthorizationHandlerContext context,
+        IsOwnerRequirement requirement,
+        Chatroom resource)
+    {
+        var user = context.User;
+        var userId = user.Claims.First(claim => claim.Type == ClaimTypes.PrimarySid).Value;
+        var chatroomOwnerId = resource.Members.First().AccountId.ToString();
+
+        if (userId == chatroomOwnerId)
         {
             context.Succeed(requirement);
         }
