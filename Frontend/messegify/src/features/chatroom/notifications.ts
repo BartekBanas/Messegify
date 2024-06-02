228f75c8ae0cb745f7c085f1b0235f2aff6a8dc1
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-06-02 02:08:37 +0200

    Add new notifications

--- Frontend/messegify/src/features/chatroom/notifications.ts
+++ Frontend/messegify/src/features/chatroom/notifications.ts
@@ -9,3 +9,39 @@ export function sendMessageErrorNotification() {
         styles: notificationStyles,
     })
 }
+
+export function InviteToChatroomSuccessNotification() {
+    showNotification({
+        color: 'green',
+        title: 'Success',
+        message: 'Successfully invited user to chatroom',
+        styles: notificationStyles,
+    });
+}
+
+export function InviteToChatroomErrorNotification() {
+    showNotification({
+        color: 'red',
+        title: 'Error',
+        message: 'An error occurred while inviting user to chatroom',
+        styles: notificationStyles,
+    })
+}
+
+export function createChatroomSuccessNotification() {
+    showNotification({
+        color: 'green',
+        title: 'Success',
+        message: 'Successfully created chatroom',
+        styles: notificationStyles,
+    });
+}
+
+export function createChatroomErrorNotification() {
+    showNotification({
+        color: 'red',
+        title: 'Error',
+        message: 'An error occurred while creating chatroom',
+        styles: notificationStyles,
+    })
+}
