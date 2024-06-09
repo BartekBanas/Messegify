0339a9da85d5c5a544c17ded27a700e04b8fd7d1
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-06-09 20:18:20 +0200

    Add new properties to Contact and its DTO

--- Backend/Messegify/Messegify.Domain.Entities/Contact.cs
+++ Backend/Messegify/Messegify.Domain.Entities/Contact.cs
@@ -6,21 +6,21 @@ namespace Messegify.Domain.Entities;
 
 public class Contact : Entity
 {
-    [Key]
-    public Guid Id { get; set; }
-    
-    [ForeignKey(nameof(FirstAccount))]
-    public Guid FirstAccountId { get; set; }
+    [Key] public Guid Id { get; set; }
+
+    [ForeignKey(nameof(FirstAccount))] public Guid FirstAccountId { get; set; }
+
     public virtual Account FirstAccount { get; set; } = null!;
 
-    [ForeignKey(nameof(SecondAccount))]
-    public Guid SecondAccountId { get; set; }
+    [ForeignKey(nameof(SecondAccount))] public Guid SecondAccountId { get; set; }
+
     public virtual Account SecondAccount { get; set; } = null!;
-    
-    [ForeignKey(nameof(ContactChatRoomId))]
-    public Guid ContactChatRoomId { get; set; }
+
+    [ForeignKey(nameof(ContactChatRoomId))] public Guid ContactChatRoomId { get; set; }
+
     public virtual Chatroom ContactChatroom { get; set; } = null!;
 
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-    public DateTime DateCreated { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)] public DateTime DateCreated { get; set; }
+
+    public bool Active { get; set; } = true;
 }
