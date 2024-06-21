3f3cd62dc7d741eeb14610fd8313dbc7fd624ec0
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-06-22 01:15:30 +0200

    Add AddToChatroomRequest and its handling

--- Backend/Messegify/Messegify.Application.Services/ChatroomRequestHandler.cs
+++ Backend/Messegify/Messegify.Application.Services/ChatroomRequestHandler.cs
@@ -20,6 +20,7 @@ public interface IChatroomRequestHandler :
     IRequestHandler<GetUserChatroomsRequest, IEnumerable<ChatRoomDto>>,
     IRequestHandler<DeleteChatroomRequest>,
     IRequestHandler<InviteToChatroomRequest>,
+    IRequestHandler<AddToChatroomRequest>,
     IRequestHandler<LeaveChatroomRequest>;
 
 public class ChatroomRequestHandler : IChatroomRequestHandler
@@ -145,6 +146,21 @@ public class ChatroomRequestHandler : IChatroomRequestHandler
         var invitedAccount = await _accountRepository.GetOneRequiredAsync(request.AccountId);
 
         chatRoom.Members.Add(new AccountChatroom { AccountId = invitedAccount.Id });
+
+        await _chatRoomRepository.SaveChangesAsync();
+    }
+
+    public async Task Handle(AddToChatroomRequest request, CancellationToken cancellationToken)
+    {
+        var user = _httpContextAccessor.HttpContext.User;
+        var chatRoom = await _chatRoomRepository.GetOneRequiredAsync(chatRoom => chatRoom.Id == request.ChatroomId, 
+            nameof(Chatroom.Members));
+
+        await _authorizationService.AuthorizeRequiredAsync(user, chatRoom, AuthorizationPolicies.IsOwnerOf);
+
+        var targetAccount = await _accountRepository.GetOneRequiredAsync(request.AccountId);
+
+        chatRoom.Members.Add(new AccountChatroom { AccountId = targetAccount.Id });
 
         await _chatRoomRepository.SaveChangesAsync();
     }
---
+++ Backend/Messegify/Messegify.Application.Services/ChatroomRequests/AddToChatroomRequest.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Messegify.Application.Dtos;
+
+namespace Messegify.Application.Services.ChatroomRequests;
+
+public class AddToChatroomRequest : IRequest
+{
+    public Guid ChatroomId { get; }
+    public Guid AccountId { get; }
+
+    public AddToChatroomRequest(Guid chatroomId, Guid accountId)
+    {
+        ChatroomId = chatroomId;
+        AccountId = accountId;
+    }
+
+    public AddToChatroomRequest(ChatroomInvite chatroomInvite)
+    {
+        ChatroomId = chatroomInvite.ChatroomId;
+        AccountId = chatroomInvite.AccountId;
+    }
+}
