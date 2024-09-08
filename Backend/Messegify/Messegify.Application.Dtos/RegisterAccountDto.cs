c8241a913b274e91be94edfba6eb85a59ad7e9cd
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-09-08 15:04:07 +0200

    Refactor RegisterAccountDto

--- Backend/Messegify/Messegify.Application.Dtos/RegisterAccountDto.cs
+++ Backend/Messegify/Messegify.Application.Dtos/RegisterAccountDto.cs
@@ -2,14 +2,14 @@
 
 namespace Messegify.Application.Dtos;
 
-public class RegisterAccountDto
+public class RegisterAccountDto(string name, string password, string email)
 {
     [Required]
-    public string Name { get; set; }
-    
+    public string Name { get; init; } = name;
+
     [Required]
-    public string Password { get; set; }
-    
+    public string Password { get; init; } = password;
+
     [Required]
-    public string Email { get; set; }
+    public string Email { get; init; } = email;
 }
